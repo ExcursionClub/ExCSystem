583514ccdfc3dbdc0f4fdeb5483179054da8de91
Tomek Fraczek <tomaszmf95@yahoo.com>
2017-06-08 21:41:31 -0700

    Moved forms to forms/MemberForms, added RFID to requested data

--- core/admin.py
+++ core/admin.py
@@ -1,71 +1,18 @@
-from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 from core.models import Member
 from core.models.GearModels import Gear
 
+from core.forms.MemberForms import MemberChangeForm, MemberCreationForm
+
+
 # Register your models here.
 admin.site.register(Gear)
 
+
 # Replace the option to create users with the option to create members
-
-
-class MemberCreationForm(forms.ModelForm):
-    """A form for creating new members. Includes all the required
-    fields, plus a repeated password."""
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
-
-    class Meta:
-        model = Member
-        fields = ('email', 'first_name', 'last_name', 'phone_number', 'rfid')
-
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-        # Save the provided password in hashed format
-        user = super(MemberCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
-
-class MemberChangeForm(forms.ModelForm):
-    """A form for updating members. Includes all the fields on
-    the user, but replaces the password field with admin's
-    password hash display field.
-    """
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = Member
-        fields = ('email',
-                  'password',
-                  'date_joined',
-                  'first_name',
-                  'last_name',
-                  'phone_number',
-                  'rfid',
-                  'is_active',
-                  'is_admin')
-
-    def clean_password(self):
-        # Regardless of what the user provides, return the initial value.
-        # This is done here, rather than on the field, because the
-        # field does not have access to the initial value
-        return self.initial["password"]
-
-
 class MemberAdmin(BaseUserAdmin):
     # The forms to add and change member instances
     form = MemberChangeForm
@@ -78,7 +25,8 @@ class MemberAdmin(BaseUserAdmin):
     list_filter = ('is_active', )
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('phone_number', 'first_name', 'last_name')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number',)}),
+        ('Club info', {'fields': ('rfid', )}),
         ('Permissions', {'fields': ('is_admin',)}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. MemberAdmin
@@ -86,10 +34,10 @@ class MemberAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'first_name', 'last_name', 'phone_number', 'password1', 'password2')}
+            'fields': ('email', 'first_name', 'last_name', 'phone_number', 'rfid', 'password1', 'password2')}
         ),
     )
-    search_fields = ('email', 'phone_number', 'first_name', 'last_name')
+    search_fields = ('email', 'phone_number', 'first_name', 'last_name', 'rfid')
     ordering = ('first_name',)
     filter_horizontal = ()
 
