15637e7bd595f4770aa78b26f3b9a3c76b81c472
Tomek Fraczek <tomaszmf95@yahoo.com>
2017-06-14 11:51:38 -0700

    Installed the kiosk and browse apps in settings

--- ExCSystem/settings.py
+++ ExCSystem/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'core.apps.CoreConfig',
+    'kiosk.apps.CoreConfig',
+    'browse.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
