601046d680d255174133d0a48a3511eb6b8bf123
Tomek Fraczek <tomaszmf95@yahoo.com>
2017-06-27 12:10:06 -0700

    Added status choices to gear

--- core/models/GearModels.py
+++ core/models/GearModels.py
@@ -5,9 +5,19 @@ class Gear(models.Model):
     """
     The base model for a piece of gear
     """
-    rfid = models.CharField(max_length=10, unique=True)
-    name = models.CharField(max_length=50)
-    is_brokem = models.BooleanField(default=False)
 
     class Meta:
         verbose_name_plural = "Gear"
+
+    rfid = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=50)
+    is_brokem = models.BooleanField(default=False)
+    status_choices = {
+        (0, "In Stock"),
+        (1, "Checked Out"),
+        (2, "Broken"),
+        (3, "Missing"),
+        (4, "Dormant"),
+        (5, "Removed"),
+    }
+    status = models.IntegerField(choices=status_choices)
