7e7af79db2c9c85d3d38d1d8ecf775ebdb49d7f6
Tomek <tomaszmf95@yahoo.com>
2017-09-08 11:01:36 -0700

    Added status checks to gear models. Fixed status choices to list

--- core/models/GearModels.py
+++ core/models/GearModels.py
@@ -11,13 +11,34 @@ class Gear(models.Model):
 
     rfid = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=50)
-    is_brokem = models.BooleanField(default=False)
-    status_choices = {
+    status_choices = [
         (0, "In Stock"),
         (1, "Checked Out"),
         (2, "Broken"),
         (3, "Missing"),
         (4, "Dormant"),
         (5, "Removed"),
-    }
+    ]
     status = models.IntegerField(choices=status_choices)
+
+    def is_available(self):
+        """Returns True if the gear is available for renting"""
+        if self.status == 0:
+            return True
+        else:
+            return False
+
+    def is_active(self):
+        """Returns True if the gear is actively in circulation (ie could be checked out in a few days)"""
+        if self.status <= 1:
+            return True
+        else:
+            return False
+
+    def is_existent(self):
+        """Returns True if the gear has not been removed or lost"""
+        if self.status <= 3:
+            return True
+        else:
+            return False
+
