7c89b456fe7f5b92654ea99ea2425b569e430f0b
Tomek <tomaszmf95@yahoo.com>
2017-10-10 16:45:22 -0700

    Added certification model

---
+++ core/models/CertificationModels.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Certification(models.Model):
+    """
+    General class for all types of certifications for renting gear or going on trips
+
+    At the time of writing, the available certifications were:
+        "SUP", "Kayaking", "Mountaineering", "Climbing"
+    """
+
+    #: The name of this certification
+    title = models.CharField(max_length=20)
+
+    #: Description of the minimum requirements needed to have this certification
+    requirements = models.TextField(verbose_name="Minimum Certification Requirements")
+
