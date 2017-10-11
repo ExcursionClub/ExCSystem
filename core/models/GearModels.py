fcededc2e1ac4a62888fe15bb0388820bfc59180
Tomek <tomaszmf95@yahoo.com>
2017-10-10 17:06:33 -0700

    Added department relation and checkout requirements to gear

--- core/models/GearModels.py
+++ core/models/GearModels.py
@@ -1,5 +1,7 @@
 from django.db import models
 from .MemberModels import Member
+from .DepartamentModels import Departament
+from .CertificationModels import Certification
 
 
 class Gear(models.Model):
@@ -13,15 +15,23 @@ class Gear(models.Model):
     rfid = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=50)
     status_choices = [
-        (0, "In Stock"),
-        (1, "Checked Out"),
-        (2, "Broken"),
-        (3, "Missing"),
-        (4, "Dormant"),
-        (5, "Removed"),
+        (0, "In Stock"),        # Ready and available in the gear sheds, waiting to be used
+        (1, "Checked Out"),     # Somebody has it right now, but it should soon be available again
+        (2, "Broken"),          # It is broken to the point it should not be checked out, waiting for repair
+        (3, "Missing"),         # Has been checked out for a while, time to yell at a member to give it back
+        (4, "Dormant"),         # Missing for very long time: assume it has been lost until found
+        (5, "Removed"),         # It is gone, dead and never coming back. Always set manually
     ]
+    #: The status determines what transactions the gear can participate in and where it is visible
     status = models.IntegerField(choices=status_choices)
 
+    #: The department to which this gear belongs (roughly corresponds to STL positions)
+    department = models.ManyToManyField(Departament)
+
+    #: All the certifications that a member must posses to be allowed to check out this gear
+    min_required_certs = models.ManyToManyField(Certification, verbose_name="Minimal Certifications Required for Rental")
+
+    #: Who currently has this piece of gear. If null, then the gear is not checked out
     checked_out_to = models.ForeignKey(Member, null=True, on_delete=models.SET_NULL)
 
     def is_available(self):
