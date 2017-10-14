64e4605a5271298a771736f4fcedb70baf62fbb2
Tomek <tomaszmf95@yahoo.com>
2017-10-14 14:39:25 -0700

    Added and improved the documentation

--- core/models/TransactionModels.py
+++ core/models/TransactionModels.py
@@ -6,9 +6,8 @@ from core.models.GearModels import Gear
 
 
 def validate_auth(authorizer):
-    """
-    Makes sure that the person who authorized the transaction is in fact authorized to do so
-    """
+    """Make sure that the person who authorized the transaction is in fact authorized to do so."""
+    # TODO: should this be given a second argument of min status level?
 
     # If the member is not a staffer, then they are not allowed to authorize a transaction like this
     if not authorizer.is_staff:
@@ -16,13 +15,13 @@ def validate_auth(authorizer):
 
 
 def validate_available(gear):
-    """Ensures that the piece of gear is in fact available for checkout in the system"""
+    """Ensure that the piece of gear is in fact available for checkout (is in stock)."""
     if not gear.is_available():
         raise ValidationError("This piece of gear [{}] is not available for checkout".format(gear.rfid))
 
 
 def validate_rfid(rfid):
-    """Ensures that the given rfid is unique"""
+    """Ensure that the given rfid is unique across all tables containing rfids."""
     # TODO: is there a better way to do this? This approach might get slow
     member_rfids = [member.rfid for member in Member.objects.all()]
     if rfid in member_rfids:
@@ -36,12 +35,7 @@ def validate_rfid(rfid):
 
 
 def validate_required_certs(member, gear):
-    """
-    Validates that the member has the certifications required to check out this piece of gear
-
-    :param member: member interested in checking out this piece of gear
-    :param gear: piece of gear the member is trying to check out
-    """
+    """Validate that the member has all the certifications required to check out this piece of gear."""
     has_all_perms = True
     for cert_required in gear.min_required_certs.all():
         if cert_required not in member.certifications.all():
@@ -51,14 +45,21 @@ def validate_required_certs(member, gear):
 
 
 class TransactionManager(models.Manager):
+    """
+    Manages Transactions, and ensures their effects are implemented.
+
+    Every modification to any piece of gear should be done through a TransactionManager function, since transactions are
+    intended to track any and all state changes of any piece of gear. The transaction creation functions also
+    immediately implement the changes described by the transaction.
+    """
 
     def __make_transaction(self, authorizer_rfid, type, gear, member=None, comments=""):
         """
-        Basic transaction creation function, with the universal validators
+        Make a transaction of any type in a safe and centralized way.
 
+        :param authorizer_rfid: string, the rfid of the entity authorizing the transaction
         :param type: the type of the transaction
         :param gear: a gear model instance, the piece of gear that is being referred to
-        :param authorizer: string, the rfid of the entity authorizing the transaction
         :param member: if applicable, the member referenced in the transaction (default=None)
         :param comments: any extra comments about the transaction (default="")
         :return: Transaction
@@ -78,7 +79,11 @@ class TransactionManager(models.Manager):
 
     def make_checkout(self, authorizer_rfid, gear_rfid, member_rfid, return_date):
         """
-        Central function for creating checkout type transactions. This is the only way gear should be checked out
+        Check out a piece of gear to a member and create a transaction logging the checkout.
+
+        To check out a piece of gear, this function sets the status of that piece of gear to 1 ("Checked Out") and sets
+        the ForeignKey on gear "checked_out_to" to the member that piece of gear is being checked out to. The comment
+        is a note on when the gear is due back.
 
         :param gear_rfid: string, the 10-digit rfid of the gear being checked out
         :param member_rfid: string, the 10-digit rfid of the member checking out the gear
@@ -104,15 +109,20 @@ class TransactionManager(models.Manager):
         gear.save()
         return transaction
 
-    def add_gear(self, authorizer_rfid, gear_rfid, gear_name, gear_department, *required_certs):
+    def add_gear(self, authorizer_rfid, gear_rfid, gear_name, gear_department, *required_certs, is_new=True):
         """
-        Central function for creating Gear. This is the only way gear should ever be created
+        Create a new piece of gear and create a transaction logging the addition.
+
+        Add a brand new piece of gear to the system, ensuring that the RFID is unique across all tables containing
+        RFIDs. See GearModel for more information on the data supplied to Gear creation.
 
         :param authorizer_rfid: string, the 10-digit rfid of entity authorizing the transaction (should be staffer)\
         :param gear_rfid: string, the 10-digit rfid of the gear being added
         :param gear_name: string, the name of the piece of gear to be checked out
         :param gear_department: the department this gear belongs in
         :param required_certs: a list of the minimum certifications required to check this piece of gear out
+        :param is_new: bool, notes whether this piece of gear was just acquired by the club. If the piece of gear is not
+            newly acquired by the club, then it might be a piece of gear that lost it's tag!
         :return: Transaction (the transaction logging this gear addition), gear (the new piece of gear)
         """
         # First must make sure that the rfid is not in use by anything
@@ -124,10 +134,14 @@ class TransactionManager(models.Manager):
         for cert in required_certs:
             gear.min_required_certs.add(cert)
         gear.save()
+        if is_new:
+            comment = "Newly Acquired"
+        else:
+            comment = "Old Gear"
 
         # Make the transaction. This will also run all the necessary validations
         try:
-            transaction = self.__make_transaction(authorizer_rfid, "Create", gear, comments="")
+            transaction = self.__make_transaction(authorizer_rfid, "Create", gear, comments=comment)
         # If any validation failed, we need to undo the gear creation before aborting
         except ValidationError:
             gear.delete()
@@ -139,7 +153,10 @@ class TransactionManager(models.Manager):
 
     def check_in_gear(self, authorizer_rfid, gear_rfid):
         """
-        Central function for checking in gear. All gear check-ins must pass through here
+        Check in a piece of gear and create a transaction logging the return.
+
+        Change the status of a piece of gear to In Stock (available for rental), and remove the member it is checked out
+        to. This should be called any time a previously checked out piece of gear becomes available for rental
 
         :param authorizer_rfid: string, the 10-digit rfid of the entity authorizing the transaction
         :param gear_rfid: string, 10-digit rfid of the gear being checked in
@@ -159,7 +176,10 @@ class TransactionManager(models.Manager):
 
     def retag_gear(self, authorizer_rfid, old_rfid, new_rfid):
         """
-        Central function to change the rfid of a piece of gear
+        Change the RFID of a piece of gear, and create a transaction logging the change.
+
+        If for whatever reason a piece of gear loses it's RFID tag, then the RFID of that piece of gear must be changed
+        in the system, but the rest of the properties of that piece of gear should not be changed.
 
         :param authorizer_rfid: string, the 10-digit rfid of the entity authorizing the transaction
         :param old_rfid: string, the old 10-digit rfid of the piece of gear
@@ -179,19 +199,25 @@ class TransactionManager(models.Manager):
 
         return transaction
 
-    def fix_gear(self, authorizer_rfid, gear_rfid, repairs_description):
+    def fix_gear(self, authorizer_rfid, gear_rfid, repairs_description, person_repairing):
         """
-        Central function for noting that a piece of gear was repaired
+        Note that a broken piece of gear was fixed and has been placed back in circulation
+
+        If a piece of gear has been repaired sufficiently to be again usable, the repairs and the person doing the
+        repairs should be noted, and the gear should be placed back in circulation
 
         :param authorizer_rfid: string, the 10-digit rfid of the entity authorizing the transaction
         :param gear_rfid: string, 10-digit rfid of the gear that got broken
         :param repairs_description: a description of the repairs that were made
+        :param person_repairing: the name of the person who preformed the repairs
         :return: Fix Transaction
         """
         gear = Gear.objects.get(rfid=gear_rfid)
 
+        comment= "{} {}".format(person_repairing, repairs_description)
+
         # Create a transaction to ensure everything is authorized
-        transaction = self.__make_transaction(authorizer_rfid, "Fix", gear, comments=repairs_description)
+        transaction = self.__make_transaction(authorizer_rfid, "Fix", gear, comments=comment)
 
         gear.set_status = 0
         gear.save()
@@ -199,7 +225,11 @@ class TransactionManager(models.Manager):
 
     def break_gear(self, authorizer_rfid, gear_rfid, damage_description):
         """
-        Central function for noting damage to gear
+        Note that a piece of gear is damaged and has been removed from circulation
+
+        When a piece of gear is returned or otherwise found to be in a state such that it is too broken to be used, it
+        should be noted as broken and removed from circulation until it is fixed. The STL should not be notified every
+        time a piece of gear breaks, but should be notified every now and then.
 
         :param authorizer_rfid: string, the 10-digit rfid of the entity authorizing the transaction
         :param gear_rfid: string, 10-digit rfid of the gear that got broken
@@ -218,7 +248,10 @@ class TransactionManager(models.Manager):
 
     def missing_gear(self, authorizer_rfid, gear_rfid):
         """
-        Central function for noting that a piece of gear has been missing for a while
+        Note that a piece of gear has been missing for a while and should be searched for
+
+        When a piece of gear has not been returned past it's due date, it is noted as missing. This gear will then be
+        searched for via gear-calls or some other such method.
 
         :param authorizer_rfid: string, the 10-digit rfid of the entity authorizing the transaction
         :param gear_rfid: string, 10-digit rfid of the gear that got broken
@@ -237,7 +270,11 @@ class TransactionManager(models.Manager):
 
     def expire_gear(self, authorizer_rfid, gear_rfid):
         """
-        Central function for noting that a piece of gear is probably lost
+        Note that a piece of gear has been missing for a very long time and is probably permanently lost
+
+        When a piece of gear has been missing for a very long time, it becomes reasonable to assume that it will not be
+        returned. This piece of gear should not be forgotten, but it should not be kept in the list of gear being
+        actively searched for.
 
         :param authorizer_rfid: string, the 10-digit rfid of the entity authorizing the transaction
         :param gear_rfid: string, 10-digit rfid of the gear that got broken
@@ -256,7 +293,7 @@ class TransactionManager(models.Manager):
 
     def delete_gear(self, authorizer_rfid, gear_rfid, reason):
         """
-        Central function for permanently removing a gear from circulation
+        Permanently remove a piece of gear from circulation
 
         This should only be used when a piece of gear is being throw away. Since this is a dramatic action, the STL
         for the relevant department should get an email notifying them of the removal, in case it was not intentional
@@ -316,7 +353,11 @@ class TransactionManager(models.Manager):
 
 class Transaction(models.Model):
     """
-    Model that stores the data for every transaction, to allow better monitoring of the status of the whole system
+    Model that stores the data for every transaction, to allow better monitoring of the status of the whole system.
+
+    Each change to any piece of gear should be done via a transaction manager function. This allows transactions to
+    serve as state change vectors for the system, and the status of the system at any time could be reconstructed from
+    the addition (subsequent application) of these transactions.
 
     NEVER CREATE TRANSACTIONS MANUALLY, ALWAYS USE THE MANAGER FUNCTIONS
     """
