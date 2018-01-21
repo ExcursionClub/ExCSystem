823cb95642d0504b31daf89a7207989bbc84bc36
Tomek <tomaszmf95@yahoo.com>
2018-01-21 15:44:51 -0800

    Made all models importable from the models module

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,3 +1,5 @@
-
 # Allows django to import the User (Member) classes from the models module
 from .MemberModels import MemberManager, Member
+from .GearModels import Gear
+from .TransactionModels import Transaction
+from .DepartmentModels import Department
