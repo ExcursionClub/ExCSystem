21bec6a910f2740a263feb2a506692335c9101fe
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-01-25 22:09:43 -0800

    Redirect on successful login

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', auth_views.LoginView.as_view(template_name='kiosk/login.html')),
+    path('logged_in/', views.logged_in, name='logged_in'),
 ]
--- kiosk/views.py
+++ kiosk/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, Kiosk")
+
+
+def logged_in(request):
+    return HttpResponse("Logged in")
