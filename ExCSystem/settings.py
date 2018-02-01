d076cff66f7ecc65c7d9c5801e5433b67ce43035
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-01-31 20:30:40 -0800

    Add logout btn

--- ExCSystem/settings.py
+++ ExCSystem/settings.py
@@ -126,4 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/kiosk'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- kiosk/urls.py
+++ kiosk/urls.py
@@ -5,7 +5,6 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-    #path('', views.index, name='index'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('', include('django.contrib.auth.urls')),
     path('check_out/', views.check_out, name='check_out'),
