fea5ba06b2635ba9fe4a36f4fbe6f8c8b5994bfe
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-01-31 20:12:25 -0800

    Use auth app to provide views and URLs for handling login, logout and passw man

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -1,10 +1,9 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', auth_views.LoginView.as_view(template_name='kiosk/login.html')),
-    path('logged_in/', views.logged_in, name='logged_in'),
+    path('', include('django.contrib.auth.urls')),
 ]
