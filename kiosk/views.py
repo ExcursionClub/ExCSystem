45b275fa79b5f67a0ca63723110643b6ff75b01e
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-02-12 22:51:59 -0800

    Add form to manually check out gear with rfid

--- kiosk/views.py
+++ kiosk/views.py
@@ -5,6 +5,7 @@ from django.views import generic, View
 
 from core.models.GearModels import Gear
 from core.models.MemberModels import Member
+from kiosk.CheckoutLogic import do_checkout
 from kiosk.forms import HomeForm
 
 
@@ -27,20 +28,36 @@ class CheckOutView(View):
     template_name = 'kiosk/check_out.html'
 
     def get(self, request, rfid):
+        form = HomeForm()
         name = self.get_name(rfid)
         checked_out_gear = self.get_checked_out_gear(rfid)
-        args = {'name': name, 'checked_out_gear': checked_out_gear}
+        args = {'form': form, 'name': name, 'checked_out_gear': checked_out_gear}
         return render(request, self.template_name, args)
 
-    def get_name(self, rfid):
-        name = Member.objects.filter(rfid=rfid).get().get_full_name()
+    def post(self, request, rfid):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['rfid']
+            # TODO: Check that RFID isn't already used
+            staffer_rfid = '1234567890'
+            member_rfid = rfid
+            # Get random gear RFID
+            # TODO: Add gear with known gear id for testing
+            for gear in Gear.objects.all():
+                if gear.is_available():
+                    do_checkout(staffer_rfid, member_rfid, gear.rfid)
+                    break
+            return redirect('check_out', member_rfid)
+
+    def get_name(self, member_rfid):
+        name = Member.objects.filter(rfid=member_rfid).get().get_full_name()
         if name:
             return name
         else:
             # TODO: Fail soft. Deny redirect from page
             raise ValidationError('This is not associated with a member')
 
-    def get_checked_out_gear(self, rfid):
-        current_member = Member.objects.filter(rfid=rfid).first()
+    def get_checked_out_gear(self, member_rfid):
+        current_member = Member.objects.filter(rfid=member_rfid).first()
         checked_out_gear = list(Gear.objects.filter(checked_out_to=current_member))
         return checked_out_gear
