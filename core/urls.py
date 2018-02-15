41ec3b1a810c82af4fd1d242d287ccd4cd8b743b
Tomek <tomaszmf95@yahoo.com>
2018-02-15 13:59:24 -0800

    Removed now redundant manual adds of urls

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
 from core.views.GearViews import GearDetailView
-from core.views.MemberViews import MemberDetailView
 
 urlpatterns = [
     path('gear/<int:pk>/detail/', GearDetailView.as_view(), name='core_gear_detail'),
-    path('member/<int:pk>/detail/', MemberDetailView.as_view(), name='core_member_detail'),
 ]
