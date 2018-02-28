0cfe98f7b089b8296fea12f25a2323b0d023f307
Tomek <tomaszmf95@yahoo.com>
2018-02-27 21:13:05 -0800

    Removed password from member change form

--- core/forms/MemberForms.py
+++ core/forms/MemberForms.py
@@ -194,17 +194,10 @@ class MemberChangeForm(forms.ModelForm):
     class Meta:
         model = Member
         fields = ('email',
-                  'password',
                   'first_name',
                   'last_name',
                   'phone_number',
                   'rfid',
                   'status',
                   'picture')
-
-    def clean_password(self):
-        # Regardless of what the user provides, return the initial value.
-        # This is done here, rather than on the field, because the
-        # field does not have access to the initial value
-        return self.initial["password"]
 
