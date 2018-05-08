0dbdf3328a6ad23f86127f83cc7197c58fc46bcf
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-05-07 19:44:56 -0700

    Run flake8, yapf and isort

--- RestartDatabase.py
+++ RestartDatabase.py
@@ -1,22 +1,22 @@
 """
-This script removes all traces of the old database and sets up a new one, so that the server can be run clean
+This script removes all traces of the old database and sets up a new one,
+so that the server can be run clean
 """
-
 
 import os
 import shutil
-import django
 
+import django
+# Re-initializes the database
+from django.core.management import call_command
 
 # Prepares the necessary paths
 basepath = os.getcwd()
 migrations_path = os.path.join(basepath, 'core', 'migrations')
 
-
 # Prepares python to be able to run django commands
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ExCSystem.settings.development")
 django.setup()
-
 
 # Removes all the migration files and the entire database
 try:
@@ -32,16 +32,9 @@ except FileNotFoundError:
 os.mkdir(migrations_path)
 open(os.path.join(migrations_path, '__init__.py'), 'w')
 
-
-# Re-initializes the database
-from django.core.management import call_command
 call_command('makemigrations', 'core')
 call_command('migrate')
 
 # To finish setup, run the following from the command line:
-# python3.5 manage.py createsuperuser --email=admin@excursionclubucsb.org --rfid=0000000000 --first_name=Master --last_name=admin --phone_number=+15555555555
-
-
-
-
-
+# python3.5 manage.py createsuperuser --email=admin@excursionclubucsb.org --rfid=0000000000
+# --first_name=Master --last_name=admin --phone_number=+15555555555
