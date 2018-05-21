ecfa10bc0947785871f23fe0738bf00f4a8d7cdc
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-05-21 10:49:44 -0700

    Test url and template

--- ExCSystem/tests/test_views.py
+++ ExCSystem/tests/test_views.py
@@ -9,7 +9,15 @@ class ViewTestCase(TestCase):
 
 
 class SmokeTest(ViewTestCase):
+    def test_home_page_status_code(self):
+        reseponse = self.client.get('/kiosk', follow=True)
+        self.assertStatusCode(reseponse)
+
     def test_view_url_by_name(self):
         """Redirect user to login page since not logged in"""
         response = self.client.get(reverse('home'), follow=True)
-        self.assertStatusCode(response, 200)
+        self.assertStatusCode(response)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'), follow=True)
+        self.assertTemplateUsed(response, 'registration/login.html')
