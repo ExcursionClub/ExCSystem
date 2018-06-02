763213ebb55c296889f5e1c96eec874e01e49029
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-06-02 12:01:38 -0700

    Add kiosk user login tests

---
+++ kiosk/tests/test_login.py
@@ -0,0 +1,31 @@
+from django.test import Client, TestCase
+from django.urls import reverse
+from django.utils.timezone import timedelta
+
+from core.models.MemberModels import Member
+
+
+class LoginTest(TestCase):
+    def setUp(self):
+        member = Member.objects.create_member(
+            email='testemail@test.com',
+            rfid=0o0000000001,
+            membership_duration=timedelta(days=7),
+            password='password'
+        )
+        member.save()
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('home'))
+        self.assertRedirects(response, '/kiosk/login/')
+
+    def test_user_login(self):
+        c = Client()
+        c.login(email='testemail@test.com', password='password')
+
+        response = c.get(reverse('home'))
+        # Should not redirect to login page since logged in
+        self.assertEqual(response.status_code, 200)
+
+
+        # TODO: Test if the logged in user matches 'testemail@test.com'
