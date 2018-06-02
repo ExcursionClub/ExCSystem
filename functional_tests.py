eefb49dfd0c917b189f948df9d49679c21011834
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-06-02 16:22:30 -0700

    Extend functional test using unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000/kiosk')
 
-assert 'Kiosk' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_get_browser_title(self):
+        self.browser.get('http://localhost:8000/kiosk')
+        self.assertIn('Kiosk', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
