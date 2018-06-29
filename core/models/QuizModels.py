6ad65901ca9b663506171dc2b53a7e895a073465
TomekFraczek <tomaszmf95@yahoo.com>
2018-06-28 22:46:12 -0700

    Added convinience functions to Question and Answer

--- core/models/QuizModels.py
+++ core/models/QuizModels.py
@@ -9,6 +9,13 @@ class Answer(models.Model):
     answer_phrase = models.CharField(max_length=10)
     answer_phrase.description = "Short one word summary of the answer to be used by the system"
 
+    def __str__(self):
+        return "{phrase}: {text}".format(phrase=self.answer_phrase, text=self.answer_text)
+
+    def as_choice(self):
+        """Return a tuple, as one choice tuple for the choices attribute for a char field"""
+        return tuple([self.answer_phrase, self.answer_text])
+
 
 class Question(models.Model):
 
@@ -21,5 +28,11 @@ class Question(models.Model):
     question_text = models.CharField(max_length=100)
     answers = models.ManyToManyField(to=Answer)
     correct_answer = models.ForeignKey(to=Answer, on_delete=models.CASCADE, related_name='+')
+
+    def __str__(self):
+        return self.question_text
+
+    def is_correct(self, selected_answer_phrase):
+        return selected_answer_phrase == self.correct_answer.answer_phrase
 
 
