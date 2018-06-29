030b3f3fc4f8bf75ab813f993d4570f7cdc13c26
TomekFraczek <tomaszmf95@yahoo.com>
2018-06-29 14:57:00 -0700

    Fixed status -> groups in models and admin

--- core/models/MemberModels.py
+++ core/models/MemberModels.py
@@ -162,6 +162,13 @@ class Member(AbstractBaseUser):
         else:
             return self.email
 
+    def get_group(self):
+        """Get's a string representing this members group membership"""
+        group_name = ""
+        for group in self.groups.all():
+            group_name += group.name + " "
+        return group_name
+
     def update_admin(self):
         """Updates the admin status of the user in the django system"""
         if self.groups.get(name="Admin"):
