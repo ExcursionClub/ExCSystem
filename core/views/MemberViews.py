7a77fd2e1276eccb482713788da9b9aa6f3b0bee
TomekFraczek <tomaszmf95@yahoo.com>
2018-07-03 19:58:26 -0700

    only display member detail page if they are a staffer or it's their own details

--- core/views/MemberViews.py
+++ core/views/MemberViews.py
@@ -2,6 +2,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView
 from django.utils import timezone
 from django.urls import reverse
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 from core.models.MemberModels import Member
 
@@ -25,11 +26,17 @@ def get_default_context(obj, context):
     return context
 
 
-class MemberDetailView(DetailView):
+class MemberDetailView(DetailView, UserPassesTestMixin):
     """Simple view that displays the all details of a user and provides access to specific change forms"""
 
     model = Member
     template_name = "admin/core/member/member_detail.html"
+
+    def test_func(self):
+        """Only allow members to see the detail page if it is for themselves, or they are staffers"""
+        is_staffer = self.request.user.is_staffer()
+        is_self = self.request.user.rfid == self.get_object().rfid
+        return is_staffer or is_self
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
