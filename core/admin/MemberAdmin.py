f5b4fd18df3231b0b81bc8cf9b302e9edb78f17c
TomekFraczek <tomaszmf95@yahoo.com>
2018-07-24 23:17:03 -0700

    Re-added missing member finish url

--- core/admin/MemberAdmin.py
+++ core/admin/MemberAdmin.py
@@ -47,6 +47,27 @@ class MemberAdmin(ViewableModelAdmin, BaseUserAdmin):
     list_view = MemberListView
     detail_view = MemberDetailView
 
+    def get_urls(self):
+        """Get all the urls admin related urls for member. Overridden here to add the detail view url"""
+
+        # Using this wrapper to call the view allows us to check permissions
+        def wrap(view):
+            def wrapper(*args, **kwargs):
+                return self.admin_site.admin_view(view)(*args, **kwargs)
+            wrapper.model_admin = self
+            return update_wrapper(wrapper, view)
+
+        # Get all the urls automatically generated for a admin view of a model by django
+        urls = super().get_urls()
+
+        # Setup all the additional member finish url
+        my_urls = [
+            path('<int:pk>/finish/', wrap(MemberFinishView.as_view()), name='core_member_finish')
+        ]
+
+        # Return all of our newly created urls along with all of the defaults
+        return my_urls + urls
+
     def response_add(self, request, obj, post_url_continue=None):
         """Redirect to the detail page after saving the new member, unless we're adding another"""
         if '_addanother' not in request.POST and '_continue' not in request.POST:
