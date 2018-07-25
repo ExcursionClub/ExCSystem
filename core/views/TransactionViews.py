d5806589d87266f2b42aad0f370ae7eff47476d7
TomekFraczek <tomaszmf95@yahoo.com>
2018-07-24 18:49:09 -0700

    Added restricted transaction view

--- core/views/TransactionViews.py
+++ core/views/TransactionViews.py
@@ -1,4 +1,8 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic.detail import DetailView
+
 from core.views.ViewList import RestrictedViewList
+from core.models.TransactionModels import Transaction
 
 
 class TransactionListView(RestrictedViewList):
@@ -11,3 +15,20 @@ class TransactionListView(RestrictedViewList):
         else:
             self.restriction_filters["member_id__exact"] = self.request.user.pk
             return False
+
+
+class TransactionDetailView(UserPassesTestMixin, DetailView):
+
+    model = Transaction
+    template_name = "admin/core/member/transaction_detail.html"
+
+    def test_func(self):
+        """Can view the detail of transaction of member is a staffer or transaction involves the member"""
+        transaction_to_view = self.get_object()
+        related_to_self = self.request.user.pk == transaction_to_view.member.pk
+        is_staffer = self.request.user.is_staffer
+        return is_staffer or related_to_self
+
+    def post(self, request, *args, **kwargs):
+        """Treat post requests as get requests"""
+        return self.get(request, *args, **kwargs)
