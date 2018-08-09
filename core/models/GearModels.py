21f07b07c8c90f66e3f9ce9d8afa4e7b43f0f1a7
TomekFraczek <tomaszmf95@yahoo.com>
2018-08-09 10:58:20 -0700

    Added gear manager with gear add method

--- core/models/GearModels.py
+++ core/models/GearModels.py
@@ -1,4 +1,5 @@
 import importlib
+import json
 
 from django.db import models
 from .MemberModels import Member
@@ -161,17 +162,77 @@ class CustomDataField(models.Model):
 
 
 class GearType(models.Model):
+
+    name = models.CharField(max_length="30")
+
     #: The department to which this type of gear belongs (roughly corresponds to STL positions)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
     #:
     data_fields = models.ManyToManyField(CustomDataField)
 
+    def __str__(self):
+        return self
+
+
+class GearManager(models.Manager):
+
+    @staticmethod
+    def generate_name(gear_type, **gear_data):
+        """
+        Auto-generate a name that can (semi-uniquely) identify this piece of gear
+
+        Name will be in the form: <GearType> - <attr 1>, <attr 2>, etc...
+        """
+
+        name = f"{gear_type.name} - "
+        # Get all custom data fields for this data_type, except those that contain a reference
+        attr_fields = CustomDataField.objects.filter(gear_type=gear_type).exclude(data_type='reference')
+        attributes = []
+        for field in attr_fields:
+            attributes.append(gear_data[field.name])
+        name += ", ".join(attributes)
+        return name
+
+    def create(self, rfid, gear_type, name=None, **gear_data):
+        """
+        Create a piece of gear that contains the basic data, and all additional data specified by the gear_type
+        """
+
+        if not name:
+            name = self.generate_name(gear_type, **gear_data)
+
+        # Create a simple piece of gear without any extra gear data
+        gear = Gear(
+            rfid=rfid,
+            name=name,
+            status=0,
+            gear_type=gear_type
+        )
+
+        # Filter out any passed data that is not referenced by the gear type
+        extra_fields = CustomDataField.objects.filter(gear_type=gear_type)
+        data_dict = {
+            field.name: field.serialize(gear_data[field.name]) for field in extra_fields
+        }
+
+        # Add in the additional data as a string before saving the piece of gear
+        gear.gear_data = json.dumps(data_dict)
+        gear.save()
+
+        return gear
+
+    def add(self, rfid, name, gear_type, **gear_data):
+        """Alias for gear creation"""
+        return self.create(rfid, gear_type, name=name, **gear_data)
+
 
 class Gear(models.Model):
     """
     The base model for a piece of gear
     """
+
+    objects = GearManager
 
     class Meta:
         verbose_name_plural = "Gear"
@@ -189,9 +250,6 @@ class Gear(models.Model):
     #: The status determines what transactions the gear can participate in and where it is visible
     status = models.IntegerField(choices=status_choices)
 
-    #: The department to which this gear belongs (roughly corresponds to STL positions)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
-
     #: All the certifications that a member must posses to be allowed to check out this gear
     min_required_certs = models.ManyToManyField(Certification, verbose_name="Minimal Certifications Required for Rental")
 
@@ -202,6 +260,8 @@ class Gear(models.Model):
     due_date = models.DateField(null=True, default=None)
 
     gear_type = models.ForeignKey(GearType, on_delete=models.CASCADE)
+
+    gear_data = models.CharField(max_length=2000)
 
     # TODO: Add image of gear
 
