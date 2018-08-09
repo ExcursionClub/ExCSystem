3831ad4a58dbf114b4810f42619e8c85ae409b58
TomekFraczek <tomaszmf95@yahoo.com>
2018-08-08 23:57:26 -0700

    Cleaned up code, added comments to _build_app_dict

--- core/admin/ExcAdminSite.py
+++ core/admin/ExcAdminSite.py
@@ -5,7 +5,7 @@ from django.utils.text import capfirst
 
 
 class ExcursionAdmin(AdminSite):
-    index_template = "admin_index.html"
+    index_template = 'admin_index.html'
 
     site_header = 'Excursion Admin'
     site_title = 'Excursion Admin'
@@ -13,22 +13,30 @@ class ExcursionAdmin(AdminSite):
 
     def _build_app_dict(self, request, label=None):
         """
-        Build the app dictionary. The optional `label` parameter filters models
-        of a specific app.
+        Ensure that each model with view permissions is viewable in the admin
+
+        By default, the app_dict only includes links to the changelist_view if the user has the 'change' permission for
+        the model. Therefore, if the user has a view but not change permission, then there will be no link to the
+        changelist . We change this behaviour to include links for models with just view permissions
         """
         app_dict = {}
 
+        # Collect all the models used in either all apps, or just this app
         if label:
-            models = {
-                m: m_a for m, m_a in self._registry.items()
-                if m._meta.app_label == label
-            }
+            models = {}
+            for model, model_admin in self._registry.items():
+                if model._meta.app_label == label:
+                    models[model] = model_admin
         else:
             models = self._registry
 
+        # Add in each models part of the app_dict
         for model, model_admin in models.items():
-            app_label = model._meta.app_label
+            meta_data = model._meta
+            app_label = meta_data.app_label
+            model_name = meta_data.model_name
 
+            # If the user has no permissions for this model, just omit it entirely
             has_module_perms = model_admin.has_module_permission(request)
             if not has_module_perms:
                 continue
@@ -40,27 +48,29 @@ class ExcursionAdmin(AdminSite):
             if True not in perms.values():
                 continue
 
-            info = (app_label, model._meta.model_name)
             model_dict = {
-                'name': capfirst(model._meta.verbose_name_plural),
-                'object_name': model._meta.object_name,
+                'name': capfirst(meta_data.verbose_name_plural),
+                'object_name': meta_data.object_name,
                 'perms': perms,
             }
             # The changelist is hijacked in the viewable model admin to be the ViewList
             if perms.get('view') or perms.get('change'):
                 try:
-                    model_dict['admin_url'] = reverse('admin:%s_%s_changelist' % info, current_app=self.name)
+                    model_dict['admin_url'] = reverse(f'admin:{app_label}_{model_name}_changelist', current_app=self.name)
                 except NoReverseMatch:
                     pass
 
             if perms.get('add'):
                 try:
-                    model_dict['add_url'] = reverse('admin:%s_%s_add' % info, current_app=self.name)
+                    model_dict['add_url'] = reverse(f'admin{app_label}_{model_name}_add', current_app=self.name)
                 except NoReverseMatch:
                     pass
 
+            # Append this model's data to the app dict under the appropriate app_label
             if app_label in app_dict:
                 app_dict[app_label]['models'].append(model_dict)
+
+            # If this is the first model in this app, must add the app data in addition to the model data
             else:
                 app_dict[app_label] = {
                     'name': apps.get_app_config(app_label).verbose_name,
@@ -74,6 +84,8 @@ class ExcursionAdmin(AdminSite):
                     'models': [model_dict],
                 }
 
+        # If we're only interested in the models for a certain app, only return that subset of the dict
         if label:
             return app_dict.get(label)
+
         return app_dict
