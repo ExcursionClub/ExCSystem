4ff21d1e3121138545c155c9b93688f3775f6e58
TomekFraczek <tomaszmf95@yahoo.com>
2018-09-15 16:36:15 -0700

    Made gear data save properly

--- core/forms/GearForms.py
+++ core/forms/GearForms.py
@@ -1,3 +1,5 @@
+import json
+
 from django.forms import ModelForm
 
 from core.models.GearModels import Gear
@@ -14,4 +16,20 @@ class GearChangeForm(ModelForm):
 
         # Make gear type non-editable. Necessary to avoid data corruption
         self.fields["geartype"].disabled = True
+
+    def clean_gear_data(self):
+        """Compile the data from all the custom fields to be saved into gear_data"""
+        gear_data_dict = {}
+        original_gear_data = json.loads(self.instance.gear_data)
+        for name in self.declared_fields.keys():
+            value = self.cleaned_data[name]
+            field_data = original_gear_data[name]
+            field_data["initial"] = value
+            gear_data_dict[name] = field_data
+
+        return json.dumps(gear_data_dict)
+
+    def save(self, commit=True):
+        self.instance.gear_data = self.clean_gear_data()
+        return super(GearChangeForm, self).save(commit=commit)
 
