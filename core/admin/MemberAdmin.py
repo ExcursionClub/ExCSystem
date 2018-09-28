572e0c6224ca6998e61d722fe765da387afe0708
TomekFraczek <tomaszmf95@yahoo.com>
2018-09-27 23:00:16 -0700

    Change view will either show real change or edit profile

--- core/admin/MemberAdmin.py
+++ core/admin/MemberAdmin.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect
 from django.urls import path, reverse
 from functools import update_wrapper
 
 from core.admin.ViewableAdmin import ViewableModelAdmin
 from core.forms.MemberForms import MemberChangeForm, MemberCreationForm
-from core.views.MemberViews import (MemberDetailView, MemberFinishView, MemberListView, StafferDetailView, )
+from core.views.MemberViews import (MemberDetailView, MemberFinishView, MemberListView, MemberEditProfileView,
+                                    StafferDetailView, )
 
 
 # Replace the option to create users with the option to create members
@@ -50,23 +52,24 @@ class MemberAdmin(ViewableModelAdmin, BaseUserAdmin):
     def get_urls(self):
         """Get all the urls admin related urls for member. Overridden here to add the detail view url"""
 
-        # Using this wrapper to call the view allows us to check permissions
-        def wrap(view):
-            def wrapper(*args, **kwargs):
-                return self.admin_site.admin_view(view)(*args, **kwargs)
-            wrapper.model_admin = self
-            return update_wrapper(wrapper, view)
-
         # Get all the urls automatically generated for a admin view of a model by django
         urls = super().get_urls()
 
         # Setup all the additional member finish url
         my_urls = [
-            path('<int:pk>/finish/', wrap(MemberFinishView.as_view()), name='core_member_finish')
+            path('<int:pk>/finish/', self.wrap(MemberFinishView.as_view()), name='core_member_finish')
         ]
 
         # Return all of our newly created urls along with all of the defaults
         return my_urls + urls
+
+    # Using this wrapper to call the view allows us to check permissions
+    def wrap(self, view):
+        def wrapper(*args, **kwargs):
+            return self.admin_site.admin_view(view)(*args, **kwargs)
+
+        wrapper.model_admin = self
+        return update_wrapper(wrapper, view)
 
     def response_add(self, request, obj, post_url_continue=None):
         """Redirect to the detail page after saving the new member, unless we're adding another"""
@@ -74,6 +77,15 @@ class MemberAdmin(ViewableModelAdmin, BaseUserAdmin):
             return HttpResponseRedirect(reverse("admin:core_member_detail", kwargs={'pk': obj.pk}))
         else:
             return super(MemberAdmin, self).response_add(request, obj, post_url_continue)
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        """Determine which profile edit page should be seen by the current user"""
+        if self.request.user.has_permission("change_member"):
+            return self.changeform_view(request, object_id, form_url, extra_context)
+        elif self.request.user.pk == object_id:
+            return self.wrap(MemberEditProfileView)
+        else:
+            raise PermissionDenied
 
 
 class StafferAdmin(ViewableModelAdmin):
