eb32e6e3a659f4d84c9d030c9bf653c69c817bff
TomekFraczek <tomaszmf95@yahoo.com>
2018-09-28 22:36:15 -0700

    Made password reset be for actual admin page

--- ExCSystem/urls.py
+++ ExCSystem/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 from core.admin import admin_site
 
@@ -23,7 +24,26 @@ from core.admin import admin_site
 # admin.site.index_template = path/to/the/template.html
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
+    path(
+        'admin/password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='admin_password_reset',
+    ),
+    path(
+        'admin/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+    ),
     path('admin/', admin_site.urls),
     path('kiosk/', include('kiosk.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
