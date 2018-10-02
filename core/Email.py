162b37e3f55488c2b283bbb17aeee11485b8a200
TomekFraczek <tomaszmf95@yahoo.com>
2018-10-02 14:21:18 -0700

    Pull email data from settings, use membership email as default

--- core/Email.py
+++ core/Email.py
@@ -14,8 +14,8 @@ class ExcursionEmailBackend(EmailBackend):
 
     def __init__(self, *args, **kwargs):
         super(ExcursionEmailBackend, self).__init__(*args, **kwargs)
-        self.email = os.environ.get('DEFAULT_EMAIL')
-        self.password = os.environ.get('DEFAULT_EMAIL_PASSWORD')
+        self.email = settings.DEFAULT_EMAIL
+        self.password = settings.DEFAULT_EMAIL_PASSWORD
 
 
 class ExcPasswordResetForm(PasswordResetForm):
