b881bf5918289d8deb6767b8847dabf559c7dac2
TomekFraczek <tomaszmf95@yahoo.com>
2018-10-01 19:56:04 -0700

    Use only built in django views

--- ExCSystem/urls.py
+++ ExCSystem/urls.py
@@ -4,31 +4,31 @@ from django.conf import settings
 from django.contrib.auth import views as auth_views
 
 from core.admin import admin_site
-from core.Email import ExcPasswordResetView
+from core.Email import ExcPasswordResetForm
 
 urlpatterns = [
     path('', include('front_page.urls')),
+    path(
+        'admin/password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='admin_password_reset',
+    ),
+    path(
+        'admin/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+    ),
     path('admin/', admin_site.urls),
-    path(
-      'admin/password_reset/',
-      ExcPasswordResetView.as_view(),
-      name='admin_password_reset',
-    ),
-    path(
-      'admin/password_reset/done/',
-      auth_views.PasswordResetDoneView.as_view(),
-      name='password_reset_done',
-    ),
-    path(
-      'reset/<uidb64>/<token>/',
-      auth_views.PasswordResetConfirmView.as_view(),
-      name='password_reset_confirm',
-    ),
-    path(
-      'reset/done/',
-      auth_views.PasswordResetCompleteView.as_view(),
-      name='password_reset_complete',
-    ),
     path('kiosk/', include('kiosk.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
