5d3e33a8293f62a3e83c93b54d6e468c10b85587
TomekFraczek <tomaszmf95@yahoo.com>
2018-10-01 17:39:41 -0700

    Make admin reset urls unique, remove conflicting kiosk password urls

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('', include('django.contrib.auth.urls')),
+    path('login/', LoginView.as_view(), name='kiosk_login'),
+    path('logout/', LogoutView.as_view(), name='kiosk_logout'),
     path('<int:rfid>/', views.CheckOutView.as_view(), name='check_out'),
 ]
