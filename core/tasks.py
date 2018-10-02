f0e2bb5bb1b6caf23ebb77a239ff9a9e6400f28b
TomekFraczek <tomaszmf95@yahoo.com>
2018-10-01 23:07:34 -0700

    Began adding update listserv

--- core/tasks.py
+++ core/tasks.py
@@ -1,7 +1,23 @@
-# Create your tasks here
+
+import os
+import re
+from selenium.common import B
+import ssl
+
+
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+from core.models.MemberModels import Member
 
 @shared_task
 def test_task():
     print("Tested a task")
+
+
+def make_listserv_email_file():
+    active_members = Member.objects.filter(is_active=True)
+    emails = [member.email for member in active_members]
+
+    email_file = open("listserv_emails.txt", 'w')
+    email_file.writelines(emails)
+    email_file.close()
