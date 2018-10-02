916b13b34353feb9de54bc09e47a5e0638fabe81
TomekFraczek <tomaszmf95@yahoo.com>
2018-10-02 15:35:53 -0700

    Remove email backend bc fixed default host user and password

--- core/Email.py
+++ core/Email.py
@@ -10,14 +10,6 @@ from core.models.MemberModels import Member
 from ExCSystem import settings
 
 
-class ExcursionEmailBackend(EmailBackend):
-
-    def __init__(self, *args, **kwargs):
-        super(ExcursionEmailBackend, self).__init__(*args, **kwargs)
-        self.email = settings.DEFAULT_EMAIL
-        self.password = settings.DEFAULT_EMAIL_PASSWORD
-
-
 class ExcPasswordResetView(PasswordResetView):
     from_email = settings.MEMBERSHIP_EMAIL_HOST_USER
 
