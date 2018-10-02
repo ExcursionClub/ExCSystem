007c9ed356b827957e04f90125bbf431e1c108cd
TomekFraczek <tomaszmf95@yahoo.com>
2018-10-01 20:37:51 -0700

    Fixed appearance of kiosk in URLS by removing duplicated kiosk urls

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
-
+from django.contrib.auth.views import LogoutView, LoginView
 from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('', include('django.contrib.auth.urls')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('<int:rfid>/', views.CheckOutView.as_view(), name='check_out'),
 ]
