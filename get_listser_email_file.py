6baaa01086bbb343c2f961cc46a10d0ffa58db2a
TomekFraczek <tomaszmf95@yahoo.com>
2018-10-03 15:33:30 -0700

    Add newlines in generated listserv emails

--- get_listser_email_file.py
+++ get_listser_email_file.py
@@ -3,7 +3,7 @@ import setupDjango
 from core.models.MemberModels import Member
 
 active_members = Member.objects.filter(is_active=True)
-emails = [member.email for member in active_members]
+emails = [f"{member.email}\n" for member in active_members]
 
 email_file = open("listserv_emails.txt", 'w')
 email_file.writelines(emails)
