82fcce88df7d55b15fe2a5df0451ec584d5584c0
TomekFraczek <tomaszmf95@yahoo.com>
2018-10-07 20:05:42 -0700

    use with open for file handling

--- get_listserv_email_file.py
+++ get_listserv_email_file.py
@@ -7,9 +7,8 @@ def get_active_emails():
     active_members = Member.objects.filter(is_active=True)
     emails = [f"{member.email}\n" for member in active_members]
 
-    email_file = open("listserv_emails.txt", 'w')
-    email_file.writelines(emails)
-    email_file.close()
+    with open("listserv_emails.txt", 'w') as email_file:
+        email_file.writelines(emails)
 
 
 def update_active_group_based():
