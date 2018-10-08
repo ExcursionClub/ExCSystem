f35753e1e494cd04504d827f1c08d132ca06a654
TomekFraczek <tomaszmf95@yahoo.com>
2018-10-07 20:19:11 -0700

    Updated get listserv emails task to reference existing code

--- core/tasks.py
+++ core/tasks.py
@@ -8,16 +8,12 @@ import ssl
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from core.models.MemberModels import Member
+from get_listserv_email_file import get_active_emails
 
 @shared_task
 def test_task():
     print("Tested a task")
 
-
+@shared_task()
 def make_listserv_email_file():
-    active_members = Member.objects.filter(is_active=True)
-    emails = [member.email for member in active_members]
-
-    email_file = open("listserv_emails.txt", 'w')
-    email_file.writelines(emails)
-    email_file.close()
+    get_active_emails()
