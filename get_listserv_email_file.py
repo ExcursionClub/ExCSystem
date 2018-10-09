c33deb02fdcb582fb338f5f2da233859d7f85a90
Ubuntu <ubuntu@ip-172-31-16-171.us-west-1.compute.internal>
2018-10-09 17:47:13 +0000

    Hotfix: is_active_member is property, not callable

--- get_listserv_email_file.py
+++ get_listserv_email_file.py
@@ -5,7 +5,7 @@ from core.models.MemberModels import Member
 
 def get_active_emails():
     members = Member.objects.all()
-    emails = [f"{member.email}\n" for member in members if member.is_active_member()]
+    emails = [f"{member.email}\n" for member in members if member.is_active_member]
 
     with open("listserv_emails.txt", 'w') as email_file:
         email_file.writelines(emails)
