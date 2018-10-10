8b3311ee5df4aa87c44b6474c9e39667c7e84f80
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-10-10 14:21:59 +0200

    Redirect to front-page after login and logout

--- ExCSystem/settings/base.py
+++ ExCSystem/settings/base.py
@@ -104,5 +104,5 @@ USE_L10N = True
 USE_TZ = True
 
 LOGIN_URL = '/admin/login/'
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'front-page:home'
+LOGOUT_REDIRECT_URL = 'front-page:home'
