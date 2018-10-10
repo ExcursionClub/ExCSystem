110082d2d83e890439910e8909c45e02acbbdfb1
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-10-10 17:00:28 +0200

    Add placeholder for rfid form

--- kiosk/forms.py
+++ kiosk/forms.py
@@ -4,7 +4,7 @@ from core.models.GearModels import Gear
 
 
 class HomeForm(forms.Form):
-    rfid = forms.CharField(max_length=10)
+    rfid = forms.CharField(max_length=10, widget=forms.TextInput(attrs={'placeholder': '10 digits'}))
 
 
 class RetagGearForm(forms.ModelForm):
