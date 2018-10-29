aa1f689a4df884c72032b9c04a31468a672df4eb
TomekFraczek <tomaszmf95@yahoo.com>
2018-10-29 14:03:40 -0700

    Improve context get to pass images to image widget

--- core/forms/widgets.py
+++ core/forms/widgets.py
@@ -1,4 +1,5 @@
 from django.forms import widgets
+from django.urls import reverse
 from core.models.FileModels import AlreadyUploadedImage
 
 
@@ -34,10 +35,29 @@ class ExistingImageWidget(widgets.ChoiceWidget):
     def __init__(self, image_type, attrs=None):
         super(ExistingImageWidget, self).__init__(attrs=attrs)
         self.attrs["image_type"] = image_type
+        self.image_type = image_type
+
+    def get_available_images(self):
+        return list(AlreadyUploadedImage.objects.filter(image_type=self.image_type))
+
+    def get_context(self, name, value, attrs):
+        context = super(ExistingImageWidget, self).get_context(name, value, attrs)
+        context["image_options"] = self.get_available_images()
+        return context
 
 
 class GearImageWidget(ExistingImageWidget):
 
     def __init__(self, attrs=None):
         super(GearImageWidget, self).__init__("gear", attrs=attrs)
+
+    def get_context(self, name, value, attrs):
+        context = super(GearImageWidget, self).get_context(name, value, attrs)
+
+        select_base = reverse('gearImageSelect')
+        selection_url = f"{select_base}?_to_field=primary_key&_popup=1"
+        context['selection_url'] = selection_url
+
+        return context
+
 
