a2c192f5787cbeefdea26bf7503a487118a60982
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-11-01 17:52:02 +0100

    Use global css file

--- ExCSystem/settings/development.py
+++ ExCSystem/settings/development.py
@@ -4,7 +4,9 @@ from ExCSystem.settings.base import *
 DEBUG = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = [
+    'static'
+]
 
 DATABASES = {
     'default': {
