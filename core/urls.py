5e55bae31d55cd375a2a99b654ad3d23ddeb2655
TomekFraczek <tomaszmf95@yahoo.com>
2018-11-03 11:34:37 -0700

    Remove artifacts of trying to make add image a popup

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
 
 
-from core.views.selectViews import GearImageSelect
-
 urlpatterns = [
-    path('AlreadyUploadedImage/select/', GearImageSelect.as_view(), name="gearImageSelect")
 ]
--- core/views/selectViews.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic.list import ListView
-from core.models.FileModels import AlreadyUploadedImage
-
-
-class GearImageSelect(ListView):
-
-    model = AlreadyUploadedImage
-
-
