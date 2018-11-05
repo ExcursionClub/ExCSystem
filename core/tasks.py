bf57bc3029168e976c90ffdc1fa0fa6d5119d063
TomekFraczek <tomaszmf95@yahoo.com>
2018-11-04 23:40:12 -0800

    CLeaned up get emails, added expire members

--- core/tasks.py
+++ core/tasks.py
@@ -1,19 +1,39 @@
 
-import os
-import re
-from selenium.common import B
-import ssl
-
-
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+
+from django.utils.timezone import datetime, timedelta
+
 from core.models.MemberModels import Member
-from get_listserv_email_file import get_active_emails
+from get_listserv_email_file import *
+
 
 @shared_task
 def test_task():
     print("Tested a task")
 
+
 @shared_task()
 def make_listserv_email_file():
-    get_active_emails()
+    emails = get_active_emails()
+    write_emails(emails)
+
+
+@shared_task
+def expire_members():
+
+    now = datetime.now()
+    expires_soon_time = now + timedelta(days=7)
+
+    for member in Member.objects.all():
+
+        # Only check if the member is expired if the are active.
+        if member.is_active_member:
+
+            # Expire members
+            if member.date_expires < now:
+                member.expire()
+
+            # If members will expire soon, send them an email
+            elif member.date_expires < expires_soon_time:
+                member.send_expires_soon_email()
--- get_listserv_email_file.py
+++ get_listserv_email_file.py
@@ -6,9 +6,12 @@ from core.models.MemberModels import Member
 def get_active_emails():
     members = Member.objects.all()
     emails = [f"{member.email}\n" for member in members if member.is_active_member]
+    return emails
 
+
+def write_emails(email_list):
     with open("listserv_emails.txt", 'w') as email_file:
-        email_file.writelines(emails)
+        email_file.writelines(email_list)
 
 
 if __name__ == "__main__":
