e03415cc841d8060c201311988e02c8d55748ce4
TomekFraczek <tomaszmf95@yahoo.com>
2018-11-20 02:38:22 -0800

    Wrote view to check if passed member RFID is active

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from django.http.response import HttpResponse
+
+from core.models.MemberModels import Member
 
 # Create your views here.
+
+
+class CheckIfActiveMemberView(View):
+
+    def get(self, request, *args, **kwargs):
+        rfid = request.GET.get('rfid', '')
+        matching_members = list(Member.objects.filter(rfid=rfid))
+        num_members = len(matching_members)
+
+        if num_members and matching_members[0].is_active_member:
+            response = HttpResponse(status=200)
+        else:
+            response = HttpResponse(status=401)
+
+        return response
