0e714d83d95800f119ec43776bb38958e45ff109
TomekFraczek <tomaszmf95@yahoo.com>
2018-11-20 04:00:56 -0800

    Use manager to abstract away check

--- api/views.py
+++ api/views.py
@@ -12,15 +12,11 @@ class CheckIfActiveMemberView(View):
 
     def get(self, request, rfid, *args, **kwargs):
 
-        matching_members = list(Member.objects.filter(rfid=rfid))
-        num_members = len(matching_members)
+        valid_member = MemberRFIDCheck.objects.create(rfid=rfid)
 
-        if num_members and matching_members[0].is_active_member:
-            valid_member = True
+        if valid_member:
             response = HttpResponse(status=200)
         else:
-            valid_member = False
             response = HttpResponse(status=401)
 
-        MemberRFIDCheck.objects.create(rfid_checked=rfid, was_valid=valid_member)
         return response
