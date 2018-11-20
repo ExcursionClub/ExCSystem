ab0b4983e6b780b3879a853c37358b1fdd493cf6
TomekFraczek <tomaszmf95@yahoo.com>
2018-11-20 03:32:20 -0800

    Add model to log all RFID checks

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MemberRFIDCheck(models.Model):
+    rfid_checked = models.CharField(max_length=12)
+    was_valid = models.BooleanField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
