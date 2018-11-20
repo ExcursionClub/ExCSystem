30a9603cc819e621250086a40f94b784f94c210c
TomekFraczek <tomaszmf95@yahoo.com>
2018-11-20 03:32:41 -0800

    Whenever a request is made to RFID checks, save the result

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.views.generic.base import View
 from django.http.response import HttpResponse
 
 from core.models.MemberModels import Member
+from api.models import MemberRFIDCheck
 
 # Create your views here.
 
@@ -15,8 +16,11 @@ class CheckIfActiveMemberView(View):
         num_members = len(matching_members)
 
         if num_members and matching_members[0].is_active_member:
+            valid_member = True
             response = HttpResponse(status=200)
         else:
+            valid_member = False
             response = HttpResponse(status=401)
 
+        MemberRFIDCheck.objects.create(rfid_checked=rfid, was_valid=valid_member)
         return response
