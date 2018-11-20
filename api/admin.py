7d48fee7543cc9261a161a0ee21b1a6f0745e24d
TomekFraczek <tomaszmf95@yahoo.com>
2018-11-20 04:23:33 -0800

    register admin for rfid log, make viewable in admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+from django.contrib.admin import register
+from api.models import MemberRFIDCheck
+from api.views import RFIDCheckLogViewList, RFIDCheckLogDetailView
+from core.admin import admin_site
+from core.admin.ViewableAdmin import ViewableModelAdmin
 
-# Register your models here.
+
+class MemberRFIDLogAdmin(ViewableModelAdmin):
+
+    list_display = ('timestamp', 'rfid_checked', 'message', 'was_valid',)
+    list_view = RFIDCheckLogViewList
+    detail_view_class = RFIDCheckLogDetailView
+
+
+admin_site.register(MemberRFIDCheck, MemberRFIDLogAdmin)
