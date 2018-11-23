ed1a6e0f5b4b49df22aa9b614b7653a594b0f66a
TomekFraczek <tomaszmf95@yahoo.com>
2018-11-23 02:15:00 -0800

    Fixed gear image upload to path

--- core/models/FileModels.py
+++ core/models/FileModels.py
@@ -1,10 +1,23 @@
+from os import path
 from django.db import models
+
+
+def get_upload_path(instance, filename):
+    img_type = instance.image_type.capitalize()
+    file_format = filename.split(".")[-1]
+    name = instance.name.\
+        replace(' ', '_').\
+        replace('\\', '')
+    file_path = path.join(f"{img_type}Pics", f"{name}.{file_format}")
+    return file_path
 
 
 class AlreadyUploadedImage(models.Model):
 
     name = models.CharField(max_length=50, unique=True)
-    image = models.ImageField()
+    image = models.ImageField(
+        upload_to=get_upload_path
+    )
     upload_date = models.DateTimeField(auto_now_add=True)
 
     image_type = models.CharField(
