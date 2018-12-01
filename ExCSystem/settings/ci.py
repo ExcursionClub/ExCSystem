c462f32a5b4d7e00249f3bdb31eb8c6e0d94e7d2
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-12-01 23:14:17 +0100

    Use Postgres for CI tests

--- ExCSystem/settings/ci.py
+++ ExCSystem/settings/ci.py
@@ -11,8 +11,12 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': 5432
     }
 }
 
