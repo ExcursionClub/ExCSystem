e9119a7c09e83b14a791ed064d3cd40a43f63cd2
TomekFraczek <tomaszmf95@yahoo.com>
2018-12-03 19:27:04 -0800

    Wait until pages are loaded to continue with uploading files

--- listserv_interface.py
+++ listserv_interface.py
@@ -4,6 +4,8 @@ import os
 import re
 import ssl
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions, wait
 
 from core.models.MemberModels import Member
 
@@ -32,8 +34,15 @@ def push_to_listserv(emails_file):
         # Handle target environment that doesn't support HTTPS verification
         ssl._create_default_https_context = _create_unverified_https_context
 
-    # Login in to listserv
+    # Open login page and wait till it loads
     browser.get(settings.LISTSERV_FORM_ADDRESS)
+    wait.WebDriverWait(browser, 10).until(
+        expected_conditions.element_to_be_clickable(
+            (By.XPATH, "//form/p/input[1]")
+        )
+    )
+
+    # Submit login info
     email_field = browser.find_element_by_id("Email Address")
     email_field.clear()
     email_field.send_keys(settings.LISTSERV_USERNAME)
@@ -42,7 +51,11 @@ def push_to_listserv(emails_file):
     password_field.send_keys(settings.LISTSERV_PASSWORD)
     email_field.submit()
 
-    # TODO: need to wait for request to complete here
+    # Wait for login request to complete
+    wait.WebDriverWait(browser, 10).until(
+        expected_conditions.element_to_be_clickable(
+            (By.ID, "Input File"))
+    )
 
     # Open the email upload page and submit the form
     replace_all_button = browser.find_element_by_id("radiob")
@@ -52,6 +65,7 @@ def push_to_listserv(emails_file):
     file_upload.submit()
 
     # TODO: parse response and notify someone
+    wait.WebDriverWait(browser, 10)
 
 
 if __name__ == "__main__":
