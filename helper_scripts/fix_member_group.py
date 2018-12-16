be2eb54527756f1d4e1dc91b39f4bd7639badf4f
TomekFraczek <tomaszmf95@yahoo.com>
2018-12-16 10:45:49 -0500

    Added group mapping code (maybe temporary)

--- helper_scripts/fix_member_group.py
+++ helper_scripts/fix_member_group.py
@@ -1,12 +1,26 @@
+import helper_scripts.setup_django
+
 from core.models.MemberModels import Member
 
 
-def fix_all():
+def fix_all_group_names():
     for member in Member.objects.all():
         group_name = member.groups.all()[0].name
         member.group = group_name
         member.save()
 
 
+def map_group_to_groups():
+    """Transfers all the data from the ForeignKey group to the ManyToMany field groups"""
+    print("In map group to groups")
+    for member in Member.objects.all():
+        group_id = member.group_id
+        group_name = Group.objects.get(pk=group_id)
+        print(f"Group name: {group_name}")
+        member.groups.set(Group.objects.filter(name=group_name))
+        member.save()
+
+
+
 if __name__ == "__main__":
-    fix_all()
+    map_group_to_groups()
