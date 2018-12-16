67899a7f2b9103d3de9dcb4c56b5449a8143c005
TomekFraczek <tomaszmf95@yahoo.com>
2018-12-15 22:58:59 -0500

    made fix_member_group runnable as script and function

--- fix_member_group.py
+++ fix_member_group.py
@@ -3,8 +3,12 @@ import setup_django
 from core.models.MemberModels import Member
 
 
-for member in Member.objects.all():
-    group_name = member.groups.all()[0].name
-    member.group = group_name
-    member.save()
+def fix_all():
+    for member in Member.objects.all():
+        group_name = member.groups.all()[0].name
+        member.group = group_name
+        member.save()
 
+
+if __name__ == "__main__":
+    fix_all()
