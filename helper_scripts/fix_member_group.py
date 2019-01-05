196228373219004a4cd8c3ec074e2d50985fa12b
TomekFraczek <tomaszmf95@yahoo.com>
2019-01-05 13:41:34 -0800

    Added dummy args so group name fix runs

--- helper_scripts/fix_member_group.py
+++ helper_scripts/fix_member_group.py
@@ -3,7 +3,7 @@ import helper_scripts.setup_django
 from core.models.MemberModels import Member
 
 
-def fix_all_group_names():
+def fix_all_group_names(dummy, blank):
     for member in Member.objects.all():
         group_name = member.groups.all()[0].name
         member.group = group_name
