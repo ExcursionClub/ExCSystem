4a1570262b6c54ad0996c33f3e14805158e0cdc4
TomekFraczek <tomaszmf95@yahoo.com>
2019-01-21 21:17:59 -0800

    fix appname in api urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from api.views import CheckIfActiveMemberView
 from django.urls import include, path
 
-app_name = 'kiosk'
+app_name = 'api'
 urlpatterns = [
     path('memberRFIDcheck/<str:rfid>', CheckIfActiveMemberView.as_view(), name='memberRFIDcheck'),
 ]
