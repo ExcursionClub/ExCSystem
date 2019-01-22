1dcb41e88fc74300f2228693c9082f8b8d1811ca
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-01-22 12:33:03 +0100

    Fix access to private S3 files

--- ExCSystem/settings/storage_backends.py
+++ ExCSystem/settings/storage_backends.py
@@ -3,4 +3,6 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'
+    default_acl = 'private'
     file_overwrite = False
+    custom_domain = False
