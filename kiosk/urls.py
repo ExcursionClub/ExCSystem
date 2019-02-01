003938a1c9d0958c917f3725573b03816a829b3b
TomekFraczek <tomaszmf95@yahoo.com>
2019-02-01 12:21:14 -0800

    Fixed the way rfids are passed (int -> slug)

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -7,7 +7,7 @@ app_name = 'kiosk'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('', include('django.contrib.auth.urls')),
-    path('gear/<int:rfid>/', views.GearView.as_view(), name='gear'),
-    path('member/<int:rfid>/', views.CheckOutView.as_view(), name='check_out'),
-    path('retag-gear/<int:rfid>/', views.RetagGearView.as_view(), name='retag_gear')
+    path('gear/<slug:rfid>/', views.GearView.as_view(), name='gear'),
+    path('member/<slug:rfid>/', views.CheckOutView.as_view(), name='check_out'),
+    path('retag-gear/<slug:rfid>/', views.RetagGearView.as_view(), name='retag_gear')
 ]
