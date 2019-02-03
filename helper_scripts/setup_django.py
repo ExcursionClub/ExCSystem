0c0067c8f5c25f7899d676f9d1ec6d2b7e622882
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-02-04 00:23:58 +0100

    Setup django

--- helper_scripts/setup_django.py
+++ helper_scripts/setup_django.py
@@ -6,6 +6,8 @@ import django
 print(f"Setting up django with {os.environ.get('ENV_CONFIG')} settings")
 if os.environ.get("ENV_CONFIG") == "development":
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ExCSystem.settings.development')
+elif os.environ.get("ENV_CONFIG") == "ci":
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ExCSystem.settings.ci')
 else:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ExCSystem.settings.production')
 
