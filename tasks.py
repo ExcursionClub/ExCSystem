e559455cc8d8b12989364e00a4e3c8cf4b75acb6
TomekFraczek <tomaszmf95@yahoo.com>
2019-02-15 11:41:40 -0800

    Moved populate and reset db into helper scripts

--- tasks.py
+++ tasks.py
@@ -1,11 +1,24 @@
 import sys
 from helper_scripts import setup_django
 from core.tasks import expire_members, update_listserv
+from helper_scripts.build_permissions import build_all as build_all_perms
+
+
+def populate_database():
+    import helper_scripts.populate_database
+
+
+def reset_database():
+    import helper_scripts.restart_database
 
 
 tasks = {
     'expire_member': expire_members,
-    'update_listserv': update_listserv
+    'update_listserv': update_listserv,
+    'build_permissions': build_all_perms,
+    'populate_database': populate_database,
+    'reset_database': reset_database,
+
 }
 
 
