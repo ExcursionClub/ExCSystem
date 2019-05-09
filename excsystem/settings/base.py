1833f96f58f8ba9a44cc2f9ab237345d1b527d65
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-05-09 21:29:16 +0200

    Add clickjacking protection

--- excsystem/settings/base.py
+++ excsystem/settings/base.py
@@ -71,9 +71,6 @@ WSGI_APPLICATION = "excsystem.wsgi.application"
 
 AUTH_USER_MODEL = "core.Member"
 
-# Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
@@ -86,6 +83,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+X_FRAME_OPTIONS = "DENY"
 
 PHONENUMBER_DB_FORMAT = "INTERNATIONAL"
 PHONENUMBER_DEFAULT_FORMAT = "INTERNATIONAL"
