5c2745aef4d6d4a11037fc388d8a947f897d7603
TomekFraczek <tomaszmf95@yahoo.com>
2019-06-21 14:38:02 -0700

    Updated migrations to include new fields

--- core/migrations/0004_auto_add_staffer_fields.py
+++ core/migrations/0004_auto_add_staffer_fields.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='staffer',
             name='is_active',
-            field=models.BooleanField(null=True, default=False),
+            field=models.BooleanField(default=False, null=True),
         ),
         migrations.AlterField(
             model_name='staffer',
