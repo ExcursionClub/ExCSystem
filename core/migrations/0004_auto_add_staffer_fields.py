0bc6b0293f33dbe6cab46ac5c8370ba3edb56d96
TomekFraczek <tomaszmf95@yahoo.com>
2019-08-28 17:47:20 -0700

    Fix migration names to match server history

--- core/migrations/0004_auto_add_staffer_fields.py
+++ core/migrations/0004_auto_add_staffer_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0003_auto_fix_member_post_overhaul'),
+        ('core', '0003_auto_20190116_2153'),
     ]
 
     operations = [
